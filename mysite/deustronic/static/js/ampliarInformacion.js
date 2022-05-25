6175413abcf6a22dfcdd9fb59ad12560a4d7150a
Javier Moreno <javiermoreno@opendeusto.es>
2022-05-25 23:28:07 +0200

    cambio de estilos + doc con datos

--- mysite/deustronic/static/js/ampliarInformacion.js
+++ mysite/deustronic/static/js/ampliarInformacion.js
@@ -16,30 +16,20 @@ if(ampliar_informacion){
                 sostenibilidad son tan diversos como nuestros clientes y empleados...Por ello estamos comprometidos 
                 con las personas, la sociedad y el medio ambiente. Para disfrutar de una vida mejor y llena de 
                 oportunidades. Vayamos juntos a por el. Para implementar esta promesa de sostenibilidad 
-                
-                
                 de una forma transparente, hemos creado la etiqueta Better Way, bajo la cual, agrupamos todo lo 
-                relacionado con la sostenibilidad, la sociedad y la protección del medioambiente.Para Deustronic 
-                Components S.L. la sostenibilidad no es un proyecto, sino una actitud y un camino hacia un futuro mejor. 
+                relacionado con la sostenibilidad, la sociedad y la protección del medioambiente.
+                En los últimos años, gracias a diferentes campañas de concienciación, hemos conseguido cambiar nuestros 
+                hábitos de consumo, haciéndolos más responsables y sostenibles. Ya son muchos los hogares que contribuyen 
+                activamente a la separación de basura y cada día somos más conscientes de que esta práctica es el único modo 
+                que tenemos para preservar y, por lo tanto, de disfrutar de nuestro planeta. El reciclaje funciona en cadena y 
+                por ello, el simple hecho de tirar nuestra basura en un contenedor u otro es básico para poder disfrutar de los 
+                beneficios energéticos obtenidos en el proceso de reciclaje y hacernos sertirnos mejor al saber que la reutilización 
+                de una tonelada de papel salva la vida de 17 árboles.
                 
-                
-                Los caminos hacia la sostenibilidad son tan diversos como nuestros clientes y empleados...Por ello 
-                estamos comprometidos con las personas, la sociedad y el medio ambiente. Para disfrutar de una vida
-                mejor y llena de oportunidades. Vayamos juntos a por el. Para implementar esta promesa de 
-                
-                sostenibilidad de una forma transparente, hemos creado la etiqueta Better Way, bajo la cual, agrupamos 
-                todo lo relacionado con la sostenibilidad, la sociedad y la protección del medioambiente
-
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                aaaaaaaaaaaaaaaaaaaaaaaa
-
                 ` 
         );
         //Agrega un nuevo nodo al final de la lista del elemento texto de un elemento padre (informacion)
         informacion.appendChild(texto);
+        document.getElementById('ampliar_boton').style.visibility='hidden';
     })
 }
